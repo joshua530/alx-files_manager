4d24af6bc52c7e05cdd02ed480389593409b438e
joshua530 <joshuaomari5@gmail.com>
2023-04-06 21:21:05 +0300

    rename replay function

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ import dbClient from './utils/db';
 
 const waitConnection = () => new Promise((resolve, reject) => {
   let i = 0;
-  const repeatFct = async () => {
+  const replay = async () => {
     await setTimeout(() => {
       i += 1;
       if (i >= 10) {
         reject();
       } else if (!dbClient.isAlive()) {
-        repeatFct();
+        replay();
       } else {
         resolve();
       }
     }, 1000);
   };
-  repeatFct();
+  replay();
 });
 
 (async () => {
