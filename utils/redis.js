67c385ed99b1d3c7a94bbedfdbfd8fe191d664b6
joshua530 <joshuaomari5@gmail.com>
2023-04-06 21:18:41 +0300

    add more details to comments

--- utils/redis.js
+++ utils/redis.js
@@ -14,21 +14,32 @@ class RedisClient {
     return this.client.connected;
   }
 
-  /** set value in redis server */
+  /** set value in redis server
+   *
+   * @param key the key to set
+   * @param value the value to set
+   * @param duration how long the value should live
+   */
   async set(key, value, duration) {
     const redisSet = promisify(this.client.set).bind(this.client);
     await redisSet(key, value);
     this.client.expire(key, duration);
   }
 
-  /** fetch value from redis server */
+  /** fetch value from redis server
+   *
+   * @param key the key to unset
+  */
   async get(key) {
     const redisGet = promisify(this.client.get).bind(this.client);
     const valu = await redisGet(key);
     return valu;
   }
 
-  /** unsets key from redis server */
+  /** unsets key from redis server
+   *
+   * @param key the key to unset
+   */
   async del(key) {
     const redisDel = promisify(this.client.del).bind(this.client);
     await redisDel(key);
