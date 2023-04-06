02a0a7df288eeebcc19c0e3f457fa24828676667
joshua530 <joshuaomari5@gmail.com>
2023-04-06 21:16:32 +0300

    rearrange routes

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
-const FilesController = require('../controllers/FilesController');
+const express = require("express")
+const AppController = require("../controllers/AppController")
+const UsersController = require("../controllers/UsersController")
+const AuthController = require("../controllers/AuthController")
+const FilesController = require("../controllers/FilesController")
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
-router.get('/files/:id/data', FilesController.getFile);
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get("/connect", AuthController.getConnect)
+router.get("/disconnect", AuthController.getDisconnect)
+router.post("/files", FilesController.postUpload)
+router.get("/files/:id", FilesController.getShow)
+router.get("/files/:id/data", FilesController.getFile)
+router.get("/files", FilesController.getIndex)
+router.put("/files/:id/unpublish", FilesController.putUnpublish)
+router.put("/files/:id/publish", FilesController.putPublish)
+router.get("/stats", AppController.getStats)
+router.get("/status", AppController.getStatus)
+router.post("/users", UsersController.postNew)
+router.get("/users/me", UsersController.getMe)
 
-module.exports = router;
+module.exports = router
